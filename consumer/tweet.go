4871b4aaa73dd3931f66f6213c80ecafe23e7881
xarantolus <xarantolus@protonmail.com>
2021-04-02 07:10:11 +0200

    Abstract tweet processing to a package

---
+++ consumer/tweet.go
@@ -0,0 +1,231 @@
+package consumer
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/xarantolus/spacex-hop-bot/match"
+	"github.com/xarantolus/spacex-hop-bot/util"
+)
+
+// Process handles and retweets
+type Processor struct {
+	debug bool
+
+	client *twitter.Client
+
+	selfUser *twitter.User
+
+	seenTweets map[int64]bool
+
+	spacePeopleListID      int64
+	spacePeopleListMembers map[int64]bool
+}
+
+// NewProcessor returns a new processor with the given options
+func NewProcessor(debug bool, client *twitter.Client, selfUser *twitter.User, spacePeopleListID int64) *Processor {
+	return &Processor{
+		debug: debug,
+
+		client:   client,
+		selfUser: selfUser,
+
+		spacePeopleListID: spacePeopleListID,
+
+		seenTweets:             make(map[int64]bool),
+		spacePeopleListMembers: make(map[int64]bool),
+	}
+}
+
+// Tweet processes the given tweet and checks whether it should be retweeted.
+// Tweets that have already been seen are ignored. It is not safe for concurrent use.
+func (p *Processor) Tweet(tweet twitter.Tweet) {
+	// So now we got a tweet. There are three categories that interest us:
+	// 1. Elon Musk drops insider info about starship, e.g. as a reply.
+	//    We do not care about his other tweets, so we check if any tweet
+	//    in the reply chain matches stuff about starship
+	// 2. We find a retweet
+	// 3. We find a quoted tweet
+	// 4. We find a tweet that is about starship
+
+	if p.seenTweets[tweet.ID] || tweet.Retweeted {
+		return
+	}
+
+	// Skip our own tweets
+	if tweet.User != nil && tweet.User.ID == p.selfUser.ID {
+		return
+	}
+
+	switch {
+	case tweet.User != nil && tweet.User.ScreenName == "elonmusk":
+		// When elon drops starship info, we want to retweet it.
+		// We basically detect if the thread/tweet is about starship and
+		// retweet everything that is appropriate
+		p.thread(&tweet)
+	case tweet.QuotedStatus != nil:
+		// If someone quotes a tweet, we only check the tweet that was quoted.
+		p.Tweet(*tweet.QuotedStatus)
+	case tweet.RetweetedStatus != nil:
+		p.Tweet(*tweet.RetweetedStatus)
+	case tweet.QuotedStatusID != 0:
+		// We got a quoted status, but twitter didn't deliver it in QuotedStatus. So we skip this tweet I guess
+	case match.StarshipTweet(&tweet) && !p.isReply(&tweet) && !p.isQuestion(&tweet) && !p.isReactionGIF(&tweet):
+		// If the tweet itself is about starship, we retweet it
+		// We already filtered out replies, which is important because we don't want to
+		// retweet every question someone posts under an elon post, only those that
+		// elon responded to.
+		// Then we also filter out all tweets that tag elon musk, e.g. there could be someone
+		// just tweeting something like "Do you think xyz... @elonmusk"
+		p.retweet(&tweet, "normal matcher")
+	}
+
+	p.seenTweets[tweet.ID] = true
+}
+
+// isReply returns if the given tweet is a reply to another user
+func (p *Processor) isReply(t *twitter.Tweet) bool {
+	if t.User == nil || t.InReplyToStatusID == 0 {
+		return false
+	}
+
+	if t.User.ID != t.InReplyToUserID {
+		return true
+	}
+
+	t, _, err := p.client.Statuses.Show(t.InReplyToStatusID, &twitter.StatusShowParams{
+		TweetMode: "extended",
+	})
+	if err != nil {
+		// If something goes wrong, we just assume it is a reply
+		return true
+	}
+
+	return p.isReply(t)
+}
+
+func (p *Processor) isQuestion(tweet *twitter.Tweet) bool {
+	return strings.Index(strings.ToLower(tweet.Text()), "@") < strings.Index(tweet.Text(), "?")
+}
+
+func (p *Processor) isReactionGIF(tweet *twitter.Tweet) bool {
+	if tweet.ExtendedEntities == nil || len(tweet.ExtendedEntities.Media) != 1 {
+		return false
+	}
+
+	// Type of a GIF is animated_gif
+	return strings.Contains(tweet.ExtendedEntities.Media[0].Type, "gif")
+}
+
+// retweet retweets the given tweet, but if it fails it doesn't care
+func (p *Processor) retweet(tweet *twitter.Tweet, reason string) {
+	// If we have already retweeted a tweet, we don't try to do it again, that just leads to errors
+	if tweet.Retweeted || tweet.RetweetedStatus != nil && tweet.RetweetedStatus.Retweeted || p.debug {
+		return
+	}
+
+	_, _, err := p.client.Statuses.Retweet(tweet.ID, nil)
+	if err != nil {
+		util.LogError(err, "retweeting "+util.TweetURL(tweet))
+		return
+	}
+
+	// save tweet so we can reproduce why it was matched
+	p.saveTweet(*tweet)
+
+	// Add the user to our space people list
+	p.addSpaceMember(*tweet)
+
+	twurl := util.TweetURL(tweet)
+	log.Printf("[Twitter] Retweeted %s (%s)", twurl, reason)
+
+	// Setting Retweeted can help thread to detect that it should stop
+	tweet.Retweeted = true
+}
+
+// saveTweet appends the given tweet to a JSON file for later inspections, especially in case of wrong retweets
+func (p *Processor) saveTweet(tweet twitter.Tweet) {
+	f, err := os.OpenFile("retweeted.ndjson", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println("Opening file:", err.Error())
+		return
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(tweet)
+	if err != nil {
+		log.Println("Encoding JSON:", err.Error())
+	}
+}
+
+// addSpaceMember adds the user of the given tweet to the space people list
+func (p *Processor) addSpaceMember(tweet twitter.Tweet) {
+
+	if tweet.User == nil || p.spacePeopleListMembers[tweet.User.ID] {
+		return
+	}
+
+	p.spacePeopleListMembers[tweet.User.ID] = true
+
+	_, err := p.client.Lists.MembersCreate(&twitter.ListsMembersCreateParams{
+		ListID: p.spacePeopleListID,
+		UserID: tweet.User.ID,
+	})
+	util.LogError(err, fmt.Sprintf("adding %s to list", tweet.User.ScreenName))
+}
+
+// thread processes tweet threads and retweets everything on-topic.
+// This is useful because Elon Musk often replies to people that quote tweeted/asked a questions on his tweets
+// See this for example: https://twitter.com/elonmusk/status/1372826575293583366
+// or here: https://twitter.com/elonmusk/status/1372725108909957121
+func (p *Processor) thread(tweet *twitter.Tweet) (didRetweet bool) {
+	if tweet == nil {
+		// Just in case
+		return false
+	}
+
+	// Was that tweet interesting the last time we saw it?
+	// If yes, then we should probably retweet the next stuff.
+	// If not, we can stop here because it won't get any better
+	// (we already checked the last time if it's good)
+	if p.seenTweets[tweet.ID] || tweet.Retweeted {
+		return tweet.Retweeted
+	}
+	p.seenTweets[tweet.ID] = true
+
+	// First process the rest of the thread
+	if tweet.InReplyToStatusID != 0 {
+		// Ok, there was a reply. Check if we can do something with that
+		parent, _, err := p.client.Statuses.Show(tweet.InReplyToStatusID, &twitter.StatusShowParams{
+			IncludeEntities: twitter.Bool(false),
+			TweetMode:       "extended",
+		})
+		util.LogError(err, "tweet reply status fetch (thread)")
+
+		// If we have a matching tweet thread
+		if parent != nil && p.thread(parent) {
+			p.seenTweets[parent.ID] = true
+			p.retweet(parent, "thread: matched parent")
+			didRetweet = true
+		}
+	}
+
+	// A quoted tweet. Let's see if there's anything interesting
+	if tweet.QuotedStatusID != 0 && tweet.QuotedStatus != nil {
+		return p.thread(tweet.QuotedStatus)
+	}
+
+	// Now actually match the tweet
+	if didRetweet || match.StarshipTweet(tweet) {
+
+		p.retweet(tweet, "thread: matched")
+
+		return true
+	}
+
+	return didRetweet
+}
--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"math/rand"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/xarantolus/spacex-hop-bot/bot"
 	"github.com/xarantolus/spacex-hop-bot/config"
+	"github.com/xarantolus/spacex-hop-bot/consumer"
 	"github.com/xarantolus/spacex-hop-bot/jobs"
 	"github.com/xarantolus/spacex-hop-bot/match"
-	"github.com/xarantolus/spacex-hop-bot/util"
 )
 
 var (
@@ -93,190 +89,13 @@ func main() {
 		match.LoadSatireList(client)
 	}
 
-	var (
-		// seenTweets maps the tweet id to an boolean. If the tweet was already processed/seen, it is put here
-		seenTweets = make(map[int64]bool)
-	)
+	const spacePeopleListID = 1375480259840212997
+
+	// proc handles tweets by filtering & retweeting the interesting ones
+	var proc = consumer.NewProcessor(*flagDebug, client, selfUser, spacePeopleListID)
 
 	// Now we just pass all tweets to processTweet
 	for tweet := range tweetChan {
-		processTweet(client, seenTweets, selfUser, tweet)
+		proc.Tweet(tweet)
 	}
-}
-
-func processTweet(client *twitter.Client, seenTweets map[int64]bool, selfUser *twitter.User, tweet twitter.Tweet) {
-	// So now we got a tweet. There are three categories that interest us:
-	// 1. Elon Musk drops insider info about starship, e.g. as a reply.
-	//    We do not care about his other tweets, so we check if any tweet
-	//    in the reply chain matches stuff about starship
-	// 2. We find a retweet
-	// 3. We find a quoted tweet
-	// 4. We find a tweet that is about starship
-
-	if seenTweets[tweet.ID] || tweet.Retweeted {
-		return
-	}
-
-	// Skip our own tweets
-	if tweet.User != nil && tweet.User.ID == selfUser.ID {
-		return
-	}
-
-	switch {
-	case tweet.User != nil && tweet.User.ScreenName == "elonmusk":
-		// When elon drops starship info, we want to retweet it.
-		// We basically detect if the thread/tweet is about starship and
-		// retweet everything that is appropriate
-		processThread(client, &tweet, seenTweets)
-	case tweet.QuotedStatus != nil:
-		// If someone quotes a tweet, we only check the tweet that was quoted.
-		processTweet(client, seenTweets, selfUser, *tweet.QuotedStatus)
-	case tweet.RetweetedStatus != nil:
-		processTweet(client, seenTweets, selfUser, *tweet.RetweetedStatus)
-	case tweet.QuotedStatusID != 0:
-		// We got a quoted status, but twitter didn't deliver it in QuotedStatus. So we skip this tweet I guess
-	case match.StarshipTweet(&tweet) && !isReply(client, &tweet) && !isQuestion(&tweet) && !isReactionGIF(&tweet):
-		// If the tweet itself is about starship, we retweet it
-		// We already filtered out replies, which is important because we don't want to
-		// retweet every question someone posts under an elon post, only those that
-		// elon responded to.
-		// Then we also filter out all tweets that tag elon musk, e.g. there could be someone
-		// just tweeting something like "Do you think xyz... @elonmusk"
-		retweet(client, &tweet, "normal matcher")
-	}
-
-	seenTweets[tweet.ID] = true
-}
-
-// isReply returns if the given tweet is a reply to another user
-func isReply(c *twitter.Client, t *twitter.Tweet) bool {
-	if t.User == nil || t.InReplyToStatusID == 0 {
-		return false
-	}
-
-	if t.User.ID != t.InReplyToUserID {
-		return true
-	}
-
-	t, _, err := c.Statuses.Show(t.InReplyToStatusID, &twitter.StatusShowParams{
-		TweetMode: "extended",
-	})
-	if err != nil {
-		// If something goes wrong, we just assume it is a reply
-		return true
-	}
-
-	return isReply(c, t)
-}
-
-func isQuestion(tweet *twitter.Tweet) bool {
-	return strings.Index(strings.ToLower(tweet.Text()), "@") < strings.Index(tweet.Text(), "?")
-}
-
-func isReactionGIF(tweet *twitter.Tweet) bool {
-	if tweet.ExtendedEntities == nil || len(tweet.ExtendedEntities.Media) != 1 {
-		return false
-	}
-
-	// Type of a GIF is animated_gif
-	return strings.Contains(tweet.ExtendedEntities.Media[0].Type, "gif")
-}
-
-const spacePeopleListID = 1375480259840212997
-
-var spacePeopleMembers = map[int64]bool{}
-
-// retweet retweets the given tweet, but if it fails it doesn't care
-func retweet(client *twitter.Client, tweet *twitter.Tweet, reason string) {
-	// If we have already retweeted a tweet, we don't try to do it again, that just leads to errors
-	if tweet.Retweeted || tweet.RetweetedStatus != nil && tweet.RetweetedStatus.Retweeted || *flagDebug {
-		return
-	}
-
-	_, _, err := client.Statuses.Retweet(tweet.ID, nil)
-	if err != nil {
-		util.LogError(err, "retweeting "+util.TweetURL(tweet))
-		return
-	}
-
-	// Collect interesting people in a list
-	if tweet.User != nil && !spacePeopleMembers[tweet.User.ID] {
-		spacePeopleMembers[tweet.User.ID] = true
-
-		_, err := client.Lists.MembersCreate(&twitter.ListsMembersCreateParams{
-			ListID: spacePeopleListID,
-			UserID: tweet.User.ID,
-		})
-		util.LogError(err, fmt.Sprintf("adding %s to list", tweet.User.ScreenName))
-	}
-
-	twurl := util.TweetURL(tweet)
-	log.Printf("[Twitter] Retweeted %s (%s)", twurl, reason)
-
-	f, err := os.OpenFile("retweeted.ndjson", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		log.Println("Opening file:", err.Error())
-		return
-	}
-	defer f.Close()
-
-	err = json.NewEncoder(f).Encode(tweet)
-	if err != nil {
-		log.Println("Encoding JSON:", err.Error())
-	}
-
-	// Setting Retweeted can help processThread to detect that it should stop
-	tweet.Retweeted = true
-}
-
-// processThread processes tweet threads and retweets everything on-topic.
-// This is useful because Elon Musk often replies to people that quote tweeted/asked a questions on his tweets
-// See this for example: https://twitter.com/elonmusk/status/1372826575293583366
-// or here: https://twitter.com/elonmusk/status/1372725108909957121
-func processThread(client *twitter.Client, tweet *twitter.Tweet, seenTweets map[int64]bool) (didRetweet bool) {
-	if tweet == nil {
-		// Just in case
-		return false
-	}
-
-	// Was that tweet interesting the last time we saw it?
-	// If yes, then we should probably retweet the next stuff.
-	// If not, we can stop here because it won't get any better
-	// (we already checked the last time if it's good)
-	if seenTweets[tweet.ID] || tweet.Retweeted {
-		return tweet.Retweeted
-	}
-	seenTweets[tweet.ID] = true
-
-	// First process the rest of the thread
-	if tweet.InReplyToStatusID != 0 {
-		// Ok, there was a reply. Check if we can do something with that
-		parent, _, err := client.Statuses.Show(tweet.InReplyToStatusID, &twitter.StatusShowParams{
-			IncludeEntities: twitter.Bool(false),
-			TweetMode:       "extended",
-		})
-		util.LogError(err, "tweet reply status fetch (processThread)")
-
-		// If we have a matching tweet thread
-		if parent != nil && processThread(client, parent, seenTweets) {
-			seenTweets[parent.ID] = true
-			retweet(client, parent, "processThread: matched parent")
-			didRetweet = true
-		}
-	}
-
-	// A quoted tweet. Let's see if there's anything interesting
-	if tweet.QuotedStatusID != 0 && tweet.QuotedStatus != nil {
-		return processThread(client, tweet.QuotedStatus, seenTweets)
-	}
-
-	// Now actually match the tweet
-	if didRetweet || match.StarshipTweet(tweet) {
-
-		retweet(client, tweet, "processThread: matched")
-
-		return true
-	}
-
-	return didRetweet
 }
