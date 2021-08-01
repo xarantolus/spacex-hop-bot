caa24d66367e996856c30720f25c3f9231f442a7
xarantolus <xarantolus@protonmail.com>
2021-08-01 16:08:02 +0200

    Keyword

--- match/starship.go
+++ match/starship.go
@@ -95,6 +95,9 @@ var (
 
 		"no tfr",
 
+		// "super heavyweight" in olympics...
+		"super heavyweight",
+
 		// 3d models are nice, but we only care about "real info"
 		"thanks", "thank you", "cheers", "render", "animation", "3d", "model", "speculati" /*ng/on*/, "simulated", "print", "vfx", "not real", "photoshop",
 		"artwork", "artist", "#art",
