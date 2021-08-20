a783e9724730d4ee02d37c8de3df313e911ef512
xarantolus <xarantolus@protonmail.com>
2021-08-20 15:15:44 +0200

    Keyword

--- match/starship.go
+++ match/starship.go
@@ -81,7 +81,7 @@ var (
 		"resilience", "shuttle", "new glenn", "china", "india", "chinese", "russia", "new shepard", "tsla", "dynetics", "hls",
 		"ares", "titan", "ariane", "srb", "solid rocket booster", "terran", "relativity space", "relativityspace", "astra",
 		"spaceshipthree", "spaceshiptwo", "spaceshipone", "vss enterprise", "vss imagine", "samsung", "bezos", "jeff who", "branson", "tory", "bruno",
-		"masten", "centaur", "atlas v", "relativity", "northrop grumman", "rookisaacman", "cygnus", "samsung", "angara",
+		"masten", "centaur", "atlas v", "relativity", "northrop grumman", "rookisaacman", "cygnus", "samsung", "angara", "firefly",
 
 		"amazon", "kuiper", "nasaartemis", "isro",
 
