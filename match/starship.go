fc6218f936feda83bcd19a1c0d989f57f12184c0
xarantolus <xarantolus@protonmail.com>
2021-11-16 07:57:16 +0100

    Keyword

--- match/starship.go
+++ match/starship.go
@@ -186,7 +186,7 @@ var (
 		"dearmoon", "dear moon", "inspiration4", "inspiration 4", "inspiration four", "rover", "alien",
 
 		"sale", "buy", "shop", "store", "purchase", "shirt", "sweater", "giveaway", "give away", "retweet", "birthday", "download", "click", "tag", "discount",
-		"pre-order", "merch", "vote", "podcast", "trending", "hater", "follow", "unfollow", "top friends",
+		"pre-order", "merch", "vote", "podcast", "trending", "hater", "follow", "unfollow", "top friends", "plush",
 
 		"child", "illegal", "nfl", "tiktok", "tik tok", "self harm", "sex", "cock", "s3x", "gspot", "g-spot", "fuck", "dick", "bullshit", "bikini",
 
